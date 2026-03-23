9d945f713a9ff34063a6cf5741c888225c034f68
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-03-24 01:30:47 +0530

    refactor: file structure

--- application/simply/settings.py
+++ application/simply/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--@u)%e+8o@tfhle%^np*ydyh&-1ybd3gpsl=ko)nu07w=v6n*&'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
