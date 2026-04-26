eb1e3d5c1dbbc36bf3b65cf31ff7df4e638bf9af
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-04-26 16:11:51 +0530

    update: files structure

--- application1/home/views.py
+++ application1/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
-from . forms import UserRegistrationForm
+from .forms import UserRegistrationForm
 from django.contrib import messages
 
 from django.http import HttpResponse
