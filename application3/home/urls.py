9bafff956f9268cbd27da3b45da33c1acd67e062
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-04-28 09:42:38 +0530

    update: application2 workflow

---
+++ application3/home/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('health/', health),
+    path('health/startup/', startup),
+    path('health/ready/', readiness),
+    path('health/live/', liveness),
+
+    path('', home, name = 'home'),
+
+    path('register/', register, name = 'register'),
+    path('login/', login, name = 'login'),
+    path('logout/', logout, name = 'logout'),
+]
