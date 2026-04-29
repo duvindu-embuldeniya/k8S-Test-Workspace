877ef6fed69c090b0e93c153701eafc2f0092c8b
duvindu-embuldeniya <duvindu.personal@gmail.com>
2026-04-29 16:54:32 +0530

    application3 setup

---
+++ application3/simply/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . views import *
+
+urlpatterns = [
+    path('', home, name = 'home'),
+
+    path('health/', health),
+
+    path('register/', register, name = 'register'),
+    path('login/', login, name = 'login'),
+    path('logout/', logout, name = 'logout'),
+]
